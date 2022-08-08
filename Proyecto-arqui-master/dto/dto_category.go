fb9df718621b368f489854acea2b38a66f5aaee7
FranciscoLlabres <80827568+FranciscoLlabres@users.noreply.github.com>
2022-08-08 10:41:20 -0300

    Delete Proyecto-arqui-master directory

--- Proyecto-arqui-master/dto/dto_category.go
+++
@@ -1,8 +0,0 @@
-package dto
-
-type CategoryDto struct {
-	Id   int    `json:"id_category"`
-	Name string `json:"name"`
-}
-
-type CategoriesDto []CategoryDto
--- Proyecto-arqui-master/model/category.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Category struct {
-	Id   int    `gorm:"primaryKey"`
-	Name string `gorm:"type:varchar(350);not null"`
-}
-
-type Categories []Category
